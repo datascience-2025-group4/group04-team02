56704210ef24eb5ac76d66297f27bbd73fa49adf
michi082 <michaela.gabor08(at)gmail.com>
2025-05-06 14:43:32 +0200

    test data exploration

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 See the project description here: https://github.com/maiwen-ch/2025_Data_Analysis_Topic_04_Antibody_Antigen_Interactions
 
 test
+
+
---
+++ test.py
@@ -0,0 +1,10 @@
+
+import pandas as pd
+
+# Load a dataset from your local data folder
+df = pd.read_csv("group04-team02/data/ab_ag.tsv", sep="\t")
+
+# Show the first few lines
+print(df.head())
+
+
